2ca50ab60bfd8a6b52a689576ee159d26238f0cb
Michael D <Michael D@10.3.10.238>
2021-03-08 10:02:06 -0500

    Initial Push

---
+++ frontend/src/frontend/Interface.java
@@ -0,0 +1,12 @@
+package frontend;
+
+public class Interface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Let's GOoooooooooooo");
+		
+	}
+
+}
